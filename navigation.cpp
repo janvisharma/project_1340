95d8646830df4c283822087675f4596bac0b7283
seanchok <seanchok@gmail.com>
2019-04-07 11:27:14 +0800

    create navigation subprogram to manage user selected program feature

---
+++ navigation.cpp
@@ -0,0 +1,12 @@
+#include "navigation.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void exitProgram() {
+  cout << "*****************************************************" << endl;
+  cout << "\nNord Etoile, thanks for using our employee management system. The changes made to your employee records have been fully reflected in the employee data file you loaded.\n\nWe wish you a pleasant day.\n" << endl;
+  cout << "*****************************************************" << endl;
+  exit(1);
+}
