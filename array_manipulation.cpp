eb2b16891f38df1f54279b43ce8407c9e9814749
janvi <janvi@connect.hku.hk>
2019-04-25 00:21:01 +0800

    create all functions for searching and sorting in array_manipulation

--- array_manipulation.cpp
+++ array_manipulation.cpp
@@ -16,6 +16,48 @@ bool compareEmployeesByLastName(Employee employeeA, Employee employeeB) {
   return employeeA.getLastName() <= employeeB.getLastName();
 }
 
+bool compareEmployeesByEmployeeId(Employee employeeA, Employee employeeB) {
+  return employeeA.getEmployeeId() <= employeeB.getEmployeeId();
+}
+
+bool compareEmployeesByAge(Employee employeeA, Employee employeeB) {
+  return employeeA.getAge() <= employeesB.getAge();
+}
+
+bool compareEmployeesByRole(Employee employeeA, Employee employeeB) {
+  return employeeA.getRole() <= employeeB.getRole();
+}
+
+bool compareEmployeesBySalary(Employee employeeA, Employee employeeB) {
+  return employeeA.getSalary() <= employeeB.getSalary();
+}
+
+bool compareEmployeesByPhoneNumber(Employee employeeA, Employee employeeB) {
+  return employeeA.getPhoneNumber() <= employeeB.getPhoneNumber();
+}
+
+bool compareEmployeesByIsAnEmployee(Employee employeeA, Employee employeeB) {
+  int tempA=-1,tempB=-1;
+  if(employeeA.getIsAnEmployee())
+  {
+    tempA = 1;
+  }
+  else {tempA = 0;}
+  if(employeesB.getIsAnEmployee())
+  {
+    tempB = 1;
+  }
+  else {tempB = 0;}
+
+  if(tempA>=tempB)
+  {
+    return true;
+  }
+  else {return false;}
+
+}
+
+
 // filter functions
 void filterEmployeesByFirstName(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, string searchValue) {
   convertStringToLower(searchValue);
@@ -34,6 +76,78 @@ void filterEmployeesByLastName(vector <Employee> employeesArray, vector <Employe
     string thisEmployeeLastName = employeesArray[i].getLastName();
     convertStringToLower(thisEmployeeLastName);
     if (thisEmployeeLastName == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByEmployeeId(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, string searchValue) {
+  for (int i = 0; i < employeesArray.size(); i++) {
+    string thisEmployeeEmployeeId = employeesArray[i].getEmployeeId();
+    if (thisEmployeeEmployeeId == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByAge(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, int searchValue) {
+
+  for (int i = 0; i < employeesArray.size(); i++) {
+    int thisEmployeeAge = employeesArray[i].getAge();
+    if (thisEmployeeFirstName == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByRole(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, string searchValue) {
+  convertStringToLower(searchValue);
+  for (int i = 0; i < employeesArray.size(); i++) {
+    string thisEmployeeRole = employeesArray[i].getRole();
+    convertStringToLower(thisEmployeeRole);
+    if (thisEmployeeRole == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesBySalary(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, double searchValue) {
+
+  for (int i = 0; i < employeesArray.size(); i++) {
+    double thisEmployeeSalary = employeesArray[i].getSalary();
+
+    if (thisEmployeeSalary == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByPhoneNumber(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, string searchValue) {
+  convertStringToLower(searchValue);
+  for (int i = 0; i < employeesArray.size(); i++) {
+    string thisEmployeePhoneNumber = employeesArray[i].getPhoneNumber();
+    convertStringToLower(thisEmployeePhoneNumber);
+    if (thisEmployeePhoneNumber == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByDateOfBirth(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, string searchValue) {
+  convertStringToLower(searchValue);
+  for (int i = 0; i < employeesArray.size(); i++) {
+    string thisEmployeeDateOfBirth = employeesArray[i].getDateOfBirth();
+    convertStringToLower(thisEmployeeDateOfBirth);
+    if (thisEmployeeDateOfBirth == searchValue) {
+      filteredEmployeesArray.push_back(employeesArray[i]);
+    }
+  }
+}
+
+void filterEmployeesByIsAnEmployee(vector <Employee> employeesArray, vector <Employee> & filteredEmployeesArray, bool searchValue) {
+  for (int i = 0; i < employeesArray.size(); i++) {
+    bool thisEmployeeIsAnEmployee = employeesArray[i].getIsAnEmployee();
+    if (thisEmployeeIsAnEmployee == searchValue) {
       filteredEmployeesArray.push_back(employeesArray[i]);
     }
   }
--- array_manipulation.h
+++ array_manipulation.h
@@ -2,13 +2,40 @@
 #include <vector>
 #include "employee_class.h"
 
-// comparision functions
+using namespace std;
+
+// filter functions for sorting
+void filterEmployeesByFirstName(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesByLastName(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesByEmployeeId(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesByAge(vector <Employee>, vector <Employee> &, int);
+
+void filterEmployeesByRole(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesBySalary(vector <Employee>, vector <Employee> &, double);
+
+void filterEmployeesByPhoneNumber(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesByDateOfBirth(vector <Employee>, vector <Employee> &, string);
+
+void filterEmployeesByIsAnEmployee(vector <Employee>, vector <Employee> &, bool);
+
+// comparision functions for searching
 bool compareEmployeesByFirstName(Employee, Employee);
 
 bool compareEmployeesByLastName(Employee, Employee);
 
+bool compareEmployeesByEmployeeId(Employee, Employee);
 
-// filter functions
-void filterEmployeesByFirstName(vector <Employee>, vector <Employee> &, string);
+bool compareEmployeesByAge(Employee, Employee);
 
-void filterEmployeesByLastName(vector <Employee>, vector <Employee> &, string);
+bool compareEmployeesByRole(Employee, Employee);
+
+bool compareEmployeesBySalary(Employee, Employee);
+
+bool compareEmployeesByPhoneNumber(Employee, Employee);
+
+bool compareEmployeesByIsAnEmployee(Employee, Employee);
